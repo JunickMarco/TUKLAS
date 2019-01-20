60dcef4c0268ad28144b019607e382cedd99914b
Hilary Casitas <2015081522@ust-ics.mygbiz.com>
2019-01-20 17:23:59 +0800

    AudioInputV1

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -4,6 +4,7 @@ import { NavController, Platform } from 'ionic-angular';
 import * as algoliasearch from 'algoliasearch';
 import { SpeechRecognition } from '@ionic-native/speech-recognition';
 import { ChangeDetectorRef } from '@angular/core';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -11,10 +12,10 @@ import { ChangeDetectorRef } from '@angular/core';
 })
 export class HomePage {
 //speech
-  matches: String[];
+  private matches: Array<string>;
   isRecording = false;
-  
-  public selectedItem: any;
+  selectedItem: string = "";
+  // public selectedItem: any;
   public selectedWord: any;
   client: any;
   index: any;
@@ -22,26 +23,28 @@ export class HomePage {
   ALGOLIA_APP_KEY: string = "0fdb807e39c747a9bd8ae696afb572e6";
   searchQuery: string = "";
   words = [];
-  constructor(public navCtrl: NavController, private plt: Platform, private speechRecognition: SpeechRecognition, private cd: ChangeDetectorRef) {
+
+  constructor(public navCtrl: NavController, private plt: Platform, private speechRecognition: SpeechRecognition, private cd: ChangeDetectorRef, private alertCtrl: AlertController) {
     this.client = algoliasearch(this.ALGOLIA_APP_ID, this.ALGOLIA_APP_KEY, {
       protocol: 'https:'
     });
 
     this.index = this.client.initIndex("tuklas_WORDS")
-
+    this.matches = [];
   }
+
+  
   search(event) {
     this.index.search({
-      query: this.searchQuery
+      query: this.selectedItem
     }).then((data) => {
-      console.log(data.hits);
+      console.log(data.hits); 
       this.words = data.hits;
       
     })
     
   }
 
- 
   navigateToDetails(word) {
     this.selectedWord = word;
     console.log(word);
@@ -65,42 +68,93 @@ export class HomePage {
     
   }
 
-  startListening() {
-    let options = {
-      language: 'en-US'
-    }
-    this.speechRecognition.startListening().subscribe(matches => {
-      console.log(matches)
-      this.matches = matches;
-      this.cd.detectChanges();
-    });
-    this.isRecording = true;
-  }
+  //SPEECH RECOGNITION
+  ngOnInit() {
 
-  // stopListening() { //for ios onleh
-  //   this.speechRecognition.stopListening().then(() => {
-  //     this.isRecording = false;
-  //   })
-  // }
-
-  getPermission() {
     this.speechRecognition.hasPermission()
       .then((hasPermission: boolean) => {
+
         if (!hasPermission) {
-          this.speechRecognition.requestPermission();
+          this.speechRecognition.requestPermission()
+            .then(
+              () => console.log('Granted'),
+              () => console.log('Denied')
+            )
         }
-      })
+
+      });
+
+  }
+  startListening() {
+    return new Promise<any>((resolve, reject) => {
+
+      let options = {
+        language: 'en-US'
+      }
+      this.speechRecognition.startListening().subscribe(matches => {
+        console.log(matches)
+        this.matches = matches;
+        this.cd.detectChanges();
+      });
+      this.isRecording = true;
+
+      setTimeout(() => {
+        resolve();
+      }, 5000);
+    });
   }
 
+  stopListening() { //for ios only
+    this.speechRecognition.stopListening().then(() => {
+      this.isRecording = false;
+    })
+  }
   isIos() {
     return this.plt.is('ios');
   }
+  presentConfirm() {
 
-  getItem(item) {
-    // store item in variable
-    this.selectedItem = item;
-    console.log(item);
+    var options = {
+      title: 'Choose the word',
+      message: 'Which name do you like?',
+      inputs: [],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Ok',
+          // handler: data => {
+          //   console.log(data);
+          // }
+          handler: (data: string) => {
+            console.log(data); 
+            this.selectedItem = data;
+          }
+        }
+      ]
+    };
+
+    options.inputs = [];
+
+    // adds radio button
+    for (let i = 0; i < this.matches.length; i++) {
+      options.inputs.push({ name: 'options', value: this.matches[i], label: this.matches[i], type: 'radio' });
+    }
+
+    let alert = this.alertCtrl.create(options);
+    alert.present();
 
   }
+
+  global() {
+    this.startListening().then(res => this.presentConfirm());
+  }
+
+
 
 }
