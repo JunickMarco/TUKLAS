d45e8d551bb7e06c9bb4aa7694e0b66924055ad1
JunickMarco <2015081561@ust-ics.mygbiz.com>
2019-02-14 21:42:49 +0800

    Added Filipino Language Intro, Baybayin Intro, Sentence Struct, Baybayin History

--- src/pages/baybayin-history/baybayin-history.ts
+++ src/pages/baybayin-history/baybayin-history.ts
@@ -30,6 +30,13 @@ export class BaybayinHistoryPage {
  }
 
  @ViewChild('pageTop') pageTop: Content;
+ @ViewChild(Content) content: Content;
+ @ViewChild('Baybay') Baybay: any;
+ @ViewChild('Kawi') Kawi: any;
+ @ViewChild('Sumatran') Sumatran: any;
+ @ViewChild('Sulawesi') Sulawesi: any;
+ @ViewChild('Assamese') Assamese: any;
+ @ViewChild('Cham') Cham: any;
 
  /**
   * Method to scroll to top
@@ -38,4 +45,34 @@ export class BaybayinHistoryPage {
    //scroll to page top
    this.pageTop.scrollToTop();
  }
+
+ public scrollElementBaybay() {
+  // Avoid reading the DOM directly, by using ViewChild and the target reference
+  this.content.scrollTo(0, this.Baybay.nativeElement.offsetTop, 500);
+}
+
+public scrollElementKawi() {
+  // Avoid reading the DOM directly, by using ViewChild and the target reference
+  this.content.scrollTo(0, this.Kawi.nativeElement.offsetTop, 500);
+}
+
+public scrollElementSumatran() {
+  // Avoid reading the DOM directly, by using ViewChild and the target reference
+  this.content.scrollTo(0, this.Sumatran.nativeElement.offsetTop, 500);
+}
+
+public scrollElementSulawesi() {
+  // Avoid reading the DOM directly, by using ViewChild and the target reference
+  this.content.scrollTo(0, this.Sulawesi.nativeElement.offsetTop, 500);
+}
+
+public scrollElementAssamese() {
+  // Avoid reading the DOM directly, by using ViewChild and the target reference
+  this.content.scrollTo(0, this.Assamese.nativeElement.offsetTop, 500);
+}
+
+public scrollElementCham() {
+  // Avoid reading the DOM directly, by using ViewChild and the target reference
+  this.content.scrollTo(0, this.Cham.nativeElement.offsetTop, 500);
+}
 }
--- src/pages/grammar-language/grammar-language.ts
+++ src/pages/grammar-language/grammar-language.ts
@@ -28,6 +28,15 @@ export class GrammarLanguagePage {
  }
 
  @ViewChild('pageTop') pageTop: Content;
+ @ViewChild(Content) content: Content;
+ @ViewChild('Cnt') Cnt: any;
+ @ViewChild('Filvstag') Filvstag: any;
+ @ViewChild('Earlyhist') Earlyhist: any;
+ @ViewChild('Earlydev') Earlydev: any;
+ @ViewChild('Spanishinf') Spanishinf: any;
+ @ViewChild('Philafter') Philafter: any;
+ @ViewChild('Filoff') Filoff: any;
+ @ViewChild('Filtoday') Filtoday: any;
 
  /**
   * Method to scroll to top
@@ -36,6 +45,46 @@ export class GrammarLanguagePage {
    //scroll to page top
    this.pageTop.scrollToTop();
  }
+
+ public scrollElementCnt() {
+  // Avoid reading the DOM directly, by using ViewChild and the target reference
+  this.content.scrollTo(0, this.Cnt.nativeElement.offsetTop, 500);
+}
+
+public scrollElementFilvstag() {
+  // Avoid reading the DOM directly, by using ViewChild and the target reference
+  this.content.scrollTo(0, this.Filvstag.nativeElement.offsetTop, 500);
+}
+
+public scrollElementEarlyhist() {
+  // Avoid reading the DOM directly, by using ViewChild and the target reference
+  this.content.scrollTo(0, this.Earlyhist.nativeElement.offsetTop, 500);
+}
+
+public scrollElementEarlydev() {
+  // Avoid reading the DOM directly, by using ViewChild and the target reference
+  this.content.scrollTo(0, this.Earlydev.nativeElement.offsetTop, 500);
+}
+
+public scrollElementSpanishinf() {
+  // Avoid reading the DOM directly, by using ViewChild and the target reference
+  this.content.scrollTo(0, this.Spanishinf.nativeElement.offsetTop, 500);
+}
+
+public scrollElementPhilafter() {
+  // Avoid reading the DOM directly, by using ViewChild and the target reference
+  this.content.scrollTo(0, this.Philafter.nativeElement.offsetTop, 500);
+}
+
+public scrollElementFiloff() {
+  // Avoid reading the DOM directly, by using ViewChild and the target reference
+  this.content.scrollTo(0, this.Filoff.nativeElement.offsetTop, 500);
+}
+
+public scrollElementFiltoday() {
+  // Avoid reading the DOM directly, by using ViewChild and the target reference
+  this.content.scrollTo(0, this.Filtoday.nativeElement.offsetTop, 500);
+}
 
 }
 
