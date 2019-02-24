94fc5945194e1e829893667edb9ecdb361668652
JunickMarco <2015081561@ust-ics.mygbiz.com>
2019-02-24 18:55:32 +0800

    UPDATED Colors

--- src/pages/grammar-colors/grammar-colors.ts
+++ src/pages/grammar-colors/grammar-colors.ts
@@ -30,6 +30,11 @@ export class GrammarColorsPage {
  }
 
  @ViewChild('pageTop') pageTop: Content;
+ @ViewChild(Content) content: Content;
+ @ViewChild('Basic') Basic: any;
+ @ViewChild('Dark') Dark: any;
+ @ViewChild('Light') Light: any;
+ @ViewChild('OtherC') OtherC: any;
 
  /**
   * Method to scroll to top
@@ -38,5 +43,25 @@ export class GrammarColorsPage {
    //scroll to page top
    this.pageTop.scrollToTop();
  }
+
+ public scrollElementBasic() {
+  // Avoid reading the DOM directly, by using ViewChild and the target reference
+  this.content.scrollTo(0, this.Basic.nativeElement.offsetTop, 500);
+}
+
+public scrollElementDark() {
+  // Avoid reading the DOM directly, by using ViewChild and the target reference
+  this.content.scrollTo(0, this.Dark.nativeElement.offsetTop, 500);
+}
+
+public scrollElementLight() {
+  // Avoid reading the DOM directly, by using ViewChild and the target reference
+  this.content.scrollTo(0, this.Light.nativeElement.offsetTop, 500);
+}
+
+public scrollElementOtherC() {
+  // Avoid reading the DOM directly, by using ViewChild and the target reference
+  this.content.scrollTo(0, this.OtherC.nativeElement.offsetTop, 500);
+}
 
 }
